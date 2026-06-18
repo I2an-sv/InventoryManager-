b2f15d15f13a9eb398f3f68d6a6db3db7bba749b
Ivan <ivan@example.com>
2026-06-18 03:23:54 +0200

    Add test for profit margin

--- test_logic.py
+++ test_logic.py
@@ -1,6 +1,5 @@
 import unittest
-from inventory import calculate_stock_value, calculate_discounted_price
-
+from inventory import calculate_stock_value, calculate_discounted_price, calculate_profit_margin
 
 class TestInventory(unittest.TestCase):
 
@@ -10,6 +9,8 @@ class TestInventory(unittest.TestCase):
     def test_discounted_price(self):
         self.assertEqual(calculate_discounted_price(200, 15), 170.0)
 
+    def test_profit_margin(self):
+        self.assertEqual(calculate_profit_margin(100, 150), 50.0)
 
 if __name__ == "__main__":
     unittest.main()
